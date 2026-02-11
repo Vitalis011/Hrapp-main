ae1e14877a7035f157a0a436dc15189f2383b1ab
Ugorji Chibuzor Vitalis <vitalisugorji1@gmail.com>
2026-02-11 14:47:16 +0200

    step 3 done

---
+++ src/components/Persons/PersonCard.jsx
@@ -0,0 +1,65 @@
+import './Persons.css';
+
+const PersonCard = (props) => {
+    const start = new Date(props.startDate);
+    const today = new Date();
+
+    let yearsWorked = today.getFullYear() - start.getFullYear();
+    const noAnniversaryYet =
+        today.getMonth() < start.getMonth() ||
+        (today.getMonth() === start.getMonth() && today.getDate() < start.getDate());
+
+    if (noAnniversaryYet) {
+        yearsWorked -= 1;
+    }
+
+    const monthsWorked = (today.getFullYear() - start.getFullYear()) * 12 + (today.getMonth() - start.getMonth());
+    const isProbation = monthsWorked < 6;
+    const isAnniversary = yearsWorked > 0 && yearsWorked % 5 === 0;
+
+    const getAnimalEmoji = (animal) => {
+        const map = {
+            parrot: "🦜",
+            cat: "🐈",
+            owl: "🦉",
+            dog: "🐕",
+            pig: "🐖",
+            chicken: "🐔",
+            leopard: "🐆",
+            eagle: "🦅",
+            bear: "🐻",
+            panda: "🐼",
+            parrot: "🦜",
+            owl: "🦉",
+            chicken: "🐔",
+            leopard: "🐆",
+            eagle: "🦅",
+            penguin: "🐧"
+        };
+
+        return map[animal?.toLowerCase()] || animal;
+    };
+
+    return (
+        <div className="personCard">
+            <p>Name: {props.name}</p>
+            <p>Title: {props.title}</p>
+            <p>Salary: {props.salary}</p>
+            <p>Phone: {props.phone}</p>
+            <p>Email: {props.email}</p>
+            <p>Animal: {getAnimalEmoji(props.animal)}</p>
+            <p>Start Date: {props.startDate}</p>
+            {isAnniversary && (
+                <p>🎉 Schedule recognition meeting.</p>
+            )}
+            {isProbation && (
+                <p>🔔 Schedule probation review.</p>
+            )}
+            <p>Location: {props.location}</p>
+            <p>Department: {props.department}</p>
+            <p>Skills: {props.skills.join(", ")}</p>
+        </div>
+    )
+}
+
+export default PersonCard;
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const About = () => {
+    return (
+        <>
+        <h1>information page about my HR app</h1>
+        </>
+    );
+};
+
+export default About;
